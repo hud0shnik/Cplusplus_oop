facc4677f69f971fc323eff5a72e3bc4750375c4
Danila Egorov <c4nsero@mail.ru>
2021-12-14 19:32:17 +0300

    + lab_22_2.2
    Добавил лабу2.2 22ой вариант и апдейтнул gitignore

---
+++ lab_22/lab2.2/lab2.2/lab2.2.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <queue>
+#include <ostream>
+
+using namespace std;
+
+class program
+{
+private:
+    int Price;
+
+public:
+
+    int getPrice() const { return Price; }
+    virtual void setPrice(int value) { Price = value; }
+    program() { Price = rand() % 10000; }
+    program(int Price) { Price = Price; }
+};
+
+ostream& operator<<(ostream& stream, const program& serial)
+{
+    stream << "Price: " << serial.getPrice();
+    return stream;
+}
+
+
+bool operator ==(program L, program B)
+{
+    if (L.getPrice() == B.getPrice())
+        return true;
+    else
+        return false;
+}
+
+bool operator<(program L, program B)
+{
+    if (L.getPrice() < B.getPrice())
+        return true;
+    else
+        return false;
+}
+
+template<typename T>
+void print_queue(T& q) {
+    while (!q.empty()) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    std::cout << '\n';
+}
+
+class comparator
+{
+public:
+    bool operator()(program& L, program& B)
+    {
+        return L.getPrice() < B.getPrice();
+    }
+};
+
+int main()
+{
+    program s1, s2, s3;
+
+    program Stad[3] = { s1, s2, s3 };
+
+    auto compare = [](program& L, program& b) { return L < b; };
+
+
+    priority_queue<program> q;
+    for (program n : Stad)
+        q.push(n);
+    print_queue(q);
+
+    return 0;
+}
