f9be328b620abd24a27020b723268d5f112bde1a
Danila Egorov <c4nsero@mail.ru>
2021-12-31 13:51:05 +0300

    update all 04 lab's
    Теперь всё понятнее и проще

---
+++ lab_04/lab2.2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <queue>
+#include <ostream>
+//Интеллектуальная собственность Дани
+using namespace std;
+
+class sportsman {
+private:
+    int Medals;
+public:
+
+    int getMedals() const {
+        return Medals;
+    }
+    virtual void setMedals(int value) {
+        Medals = value;
+    }
+    sportsman() {
+        Medals = rand() % 10;
+    }
+};
+
+bool operator == (sportsman L, sportsman R) {
+    if (L.getMedals() == R.getMedals()) {
+        return true;
+    }
+    return false;
+}
+
+bool operator < (sportsman L, sportsman R) {
+    if (L.getMedals() < R.getMedals()) {
+        return true;
+    }
+    return false;
+}
+ostream& operator << (ostream& stream,
+    const sportsman& s) {
+    stream << "Medals: " << s.getMedals();
+    return stream;
+}
+
+template < typename T >
+void print_queue(T& q) {
+    while (!q.empty()) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    std::cout << '\n';
+}
+
+class comparator {
+public:
+    bool operator()(sportsman& L, sportsman& R) {
+        return L.getMedals() < R.getMedals();
+    }
+};
+
+int main() {
+    sportsman first, second, third;
+    first.setMedals(777);
+
+    sportsman m[3] = {
+      first,
+      second,
+      third
+    };
+
+    auto compare = [](sportsman& L, sportsman& R) {
+        return L < R;
+    };
+
+    priority_queue < sportsman > queue;
+    for (int i = 0; i < 3; i++) {
+        queue.push(m[i]);
+    }
+
+    print_queue(queue);
+}
