8a9c9fde3a4da03b9969cfb43db208e30612a1b7
Danila Egorov <c4nsero@mail.ru>
2022-01-01 20:18:45 +0300

    update lab2.2

--- lab_22/lab2.2.cpp
+++ lab_22/lab2.2.cpp
@@ -4,73 +4,79 @@
 
 using namespace std;
 
-class program
-{
-private:
+class program {
+  private:
     int Price;
 
-public:
+  public:
 
-    int getPrice() const { return Price; }
-    virtual void setPrice(int value) { Price = value; }
-    program() { Price = rand() % 10000; }
-    program(int Price) { Price = Price; }
+    int getPrice() const {
+      return Price;
+    }
+  virtual void setPrice(int value) {
+    Price = value;
+  }
+  program() {
+    Price = rand() % 10000;
+  }
+  program(int Price) {
+    Price = Price;
+  }
 };
 
-ostream& operator<<(ostream& stream, const program& serial)
-{
-    stream << "Price: " << serial.getPrice();
-    return stream;
+ostream & operator << (ostream & stream,
+  const program & serial) {
+  stream << "Price: " << serial.getPrice();
+  return stream;
 }
 
-
-bool operator ==(program L, program B)
-{
-    if (L.getPrice() == B.getPrice())
-        return true;
-    else
-        return false;
+bool operator == (program L, program B) {
+  if (L.getPrice() == B.getPrice())
+    return true;
+  else
+    return false;
 }
 
-bool operator<(program L, program B)
-{
-    if (L.getPrice() < B.getPrice())
-        return true;
-    else
-        return false;
+bool operator < (program L, program B) {
+  if (L.getPrice() < B.getPrice())
+    return true;
+  else
+    return false;
 }
 
-template<typename T>
-void print_queue(T& q) {
+template < typename T >
+  void print_queue(T & q) {
     while (!q.empty()) {
-        cout << q.top() << " ";
-        q.pop();
+      cout << q.top() << " ";
+      q.pop();
     }
     std::cout << '\n';
-}
+  }
 
-class comparator
-{
-public:
-    bool operator()(program& L, program& B)
-    {
-        return L.getPrice() < B.getPrice();
+class comparator {
+  public:
+    bool operator()(program & L, program & B) {
+      return L.getPrice() < B.getPrice();
     }
 };
 
-int main()
-{
-    program s1, s2, s3;
+int main() {
+  program s1, s2, s3;
 
-    program Stad[3] = { s1, s2, s3 };
+  program Stad[3] = {
+    s1,
+    s2,
+    s3
+  };
 
-    auto compare = [](program& L, program& b) { return L < b; };
+  auto compare = [](program & L, program & b) {
+    return L < b;
+  };
 
+  priority_queue < program > q;
+  for (program n: Stad)
+    q.push(n);
+  print_queue(q);
 
-    priority_queue<program> q;
-    for (program n : Stad)
-        q.push(n);
-    print_queue(q);
-
-    return 0;
+  return 0;
 }
