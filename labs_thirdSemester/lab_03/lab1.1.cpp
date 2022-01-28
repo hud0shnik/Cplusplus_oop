5537647d82a58f4f788c65d04a3206532fa83f86
Danila Egorov <c4nsero@mail.ru>
2022-01-28 21:01:10 +0300

    + lab1.1

---
+++ labs_thirdSemester/lab_03/lab1.1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+template < class T >
+void push(list < T >& lst, T element) {
+    auto it = lst.begin();
+    while ((it != lst.end()) && (element > *it)) {
+        *it++;
+    }
+    if (it != lst.end())
+        lst.insert(it, element);
+    else
+        lst.push_back(element);
+}
+
+template < class T >
+T pop(list < T >& father) {
+    T temp = father.front();
+    father.pop_front();
+    return temp;
+}
+
+bool P(int element) {
+    for (int i = 2; i < element; i++) {
+        if (element % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template < class T >
+list < T > filter(list < T >& lst, bool(*ptr)(T)) {
+    list < T > son;
+    auto it = lst.begin();
+    while (it != lst.end()) {
+        if (P(*it)) {
+            son.push_back(*it);
+        }
+        *it++;
+    }
+    return son;
+}
+
+template < class T >
+void print(list < T >& lst) {
+    for (auto it = lst.begin(); it != lst.end(); *it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    return 0;
+}
